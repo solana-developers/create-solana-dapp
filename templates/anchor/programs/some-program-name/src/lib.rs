0ee3fa07600d1a8e7d3a4556f8d00efd025faca9
jpcaulfi <joe.caulfield@solana.org>
2022-07-07 15:58:52 -0400

    templating

--- templates/anchor/programs/some-program-name/src/lib.rs
+++ templates/anchor/programs/some-program-name/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod {{ dap_name_snake }} {
+pub mod {{ dapp_name_snake }} {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
