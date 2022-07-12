c4a4e401217fdb7ebbeb10cc5439b9705e69514f
jpcaulfi <joe.caulfield@solana.org>
2022-07-11 22:44:27 -0400

    repo url

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -9,14 +9,14 @@ export const githubApiUrlMap = new Map<string, string>([
     ["next", "https://api.github.com/repos/solana-developers/solana-dapp-next?ref=main"],
     ["vue", "https://api.github.com/repos/solana-developers/dapp-scaffold-vue?ref=main"],
     // ["svelte", "https://api.github.com/repos/solana-developers/dapp-scaffold-svelte?ref=main"],
-    ["anchor", "https://api.github.com/repos/jpcaulfi/create-solana-dapp/contents/templates/anchor?ref=master"],
-    ["native", "https://api.github.com/repos/jpcaulfi/create-solana-dapp/contents/templates/native?ref=master"],
+    ["anchor", "https://api.github.com/repos/solana-developers/create-solana-app/contents/templates/anchor?ref=main"],
+    ["native", "https://api.github.com/repos/solana-developers/create-solana-app/contents/templates/native?ref=main"],
 ]);
 
 export const githubCloneUrlMap = new Map<string, string>([
     ["next", "https://github.com/solana-developers/solana-dapp-next.git"],
     ["vue", "https://github.com/solana-developers/dapp-scaffold-vue.git"],
     // ["svelte", "https://github.com/solana-developers/dapp-scaffold-svelte.git"],
-    ["anchor", "https://github.com/jpcaulfi/create-solana-dapp.git"],
-    ["native", "https://github.com/jpcaulfi/create-solana-dapp.git"],
+    ["anchor", "https://github.com/solana-developers/create-solana-app.git"],
+    ["native", "https://github.com/solana-developers/create-solana-app.git"],
 ]);
