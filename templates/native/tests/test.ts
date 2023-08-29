3d53288beaae75ea76fb214718981eaf074e5cd4
John <johnzhengyuliu@gmail.com>
2023-08-29 11:58:21 -0500

    update native template test file

--- templates/native/tests/test.ts
+++ templates/native/tests/test.ts
@@ -1,40 +1,33 @@
 import {
-    Connection,
-    Keypair,
-    sendAndConfirmTransaction,
-    Transaction,
-    TransactionInstruction,
-} from '@solana/web3.js';
-
+  Connection,
+  Keypair,
+  sendAndConfirmTransaction,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
 
 function createKeypairFromFile(path: string): Keypair {
-    return Keypair.fromSecretKey(
-        Buffer.from(JSON.parse(require('fs').readFileSync(path, "utf-8")))
-    )
-};
-
+  return Keypair.fromSecretKey(
+    Buffer.from(JSON.parse(require("fs").readFileSync(path, "utf-8"))),
+  );
+}
 
 describe("hello-solana", () => {
+  const connection = new Connection(`http://127.0.0.1:8899`, "confirmed");
+  const payer = createKeypairFromFile("{{ solana_wallet }}");
+  const program = createKeypairFromFile(
+    "./program/solana/{{ dapp_name_snake }}-keypair.json",
+  );
 
-    const connection = new Connection(`http://localhost:8899`, 'confirmed');
-    const payer = createKeypairFromFile('{{ solana_wallet }}');
-    const program = createKeypairFromFile('./program/target/solana/{{ dapp_name_snake }}-keypair.json');
-  
-    it("Say hello!", async () => {
-
-        let ix = new TransactionInstruction({
-            keys: [
-                {pubkey: payer.publicKey, isSigner: true, isWritable: true}
-            ],
-            programId: program.publicKey,
-            data: Buffer.alloc(0), // No data
-        });
-
-        await sendAndConfirmTransaction(
-            connection, 
-            new Transaction().add(ix),
-            [payer]
-        );
+  it("Say hello!", async () => {
+    let ix = new TransactionInstruction({
+      keys: [{ pubkey: payer.publicKey, isSigner: true, isWritable: true }],
+      programId: program.publicKey,
+      data: Buffer.alloc(0), // No data
     });
+
+    await sendAndConfirmTransaction(connection, new Transaction().add(ix), [
+      payer,
+    ]);
   });
-  
+});
