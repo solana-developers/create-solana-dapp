ace66f2a2306a1a346ff8fe3027b7a93b21e9412
John <johnzhengyuliu@gmail.com>
2023-08-29 11:30:55 -0500

    update anchor template

--- templates/anchor/tests/some-program-name.ts
+++ templates/anchor/tests/some-program-name.ts
@@ -1,5 +1,5 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
 import { {{ dapp_name_camel_upper }} } from "../target/types/{{ dapp_name_snake }}";
 
 describe("{{ dapp_name }}", () => {
