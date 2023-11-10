6c6f5ef456a38bc1a76205e06afb3b4971caad63
nickfrosty <nfrostbutter@gmail.com>
2023-11-10 17:22:46 -0500

    feat: added a generic substitution function

--- packages/preset-common/src/index.ts
+++ packages/preset-common/src/index.ts
@@ -1,3 +1,4 @@
+export * from './utils/generic-substitutions'
 export * from './utils/application-cleanup'
 export * from './utils/get-recursive-file-contents'
 export * from './utils/package-versions'
