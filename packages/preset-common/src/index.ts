c3b0d214591591aa88b446744ed9750adffea09a
nickfrosty <nfrostbutter@gmail.com>
2023-11-10 15:30:30 -0500

    refactor: typo in file name

--- packages/preset-common/src/index.ts
+++ packages/preset-common/src/index.ts
@@ -1,5 +1,5 @@
 export * from './utils/application-cleanup'
-export * from './utils/get-gecursive-file-contents'
+export * from './utils/get-recursive-file-contents'
 export * from './utils/package-versions'
 export * from './utils/run-command'
 export * from './utils/update-ignore-entires'
