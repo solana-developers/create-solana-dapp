855a691206c98fe0af4e149578ea3c3e53166900
Bram Borggreve <beeman@beeman.nl>
2023-11-22 04:53:17 +0000

    feature: wrap up React templates

--- packages/preset-react/src/utils/wallet-adapter-dependencies.ts
+++ packages/preset-react/src/utils/wallet-adapter-dependencies.ts
@@ -5,6 +5,7 @@ export function walletAdapterDependencies(tree: Tree) {
   return addDependenciesToPackageJson(
     tree,
     {
+      '@solana/spl-token': packageVersion['@solana']['spl-token'],
       '@solana/wallet-adapter-base': packageVersion['@solana']['wallet-adapter-base'],
       '@solana/wallet-adapter-react': packageVersion['@solana']['wallet-adapter-react'],
       '@solana/wallet-adapter-react-ui': packageVersion['@solana']['wallet-adapter-react-ui'],
@@ -12,6 +13,7 @@ export function walletAdapterDependencies(tree: Tree) {
       '@solana/wallet-adapter-unsafe-burner': packageVersion['@solana']['wallet-adapter-unsafe-burner'],
       '@solana/web3.js': packageVersion['@solana']['web3.js'],
       bs58: packageVersion.bs58,
+      buffer: packageVersion.buffer,
     },
     {},
   )
