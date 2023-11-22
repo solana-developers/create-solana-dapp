e703c77d86eff08f4e34d2986fa56c2101f2657a
Bram Borggreve <beeman@beeman.nl>
2023-11-22 03:14:18 +0000

    feature: wrap up Anchor templates

--- packages/preset-anchor/src/generators/anchor-template/anchor-template-generator.spec.ts
+++ packages/preset-anchor/src/generators/anchor-template/anchor-template-generator.spec.ts
@@ -6,7 +6,7 @@ import { AnchorTemplateSchema } from './anchor-template-schema'
 
 describe('anchor-template generator', () => {
   let tree: Tree
-  const options: AnchorTemplateSchema = { name: 'test', directory: 'target', template: 'base' }
+  const options: AnchorTemplateSchema = { name: 'test', projectName: 'test', directory: 'target', template: 'base' }
 
   beforeEach(() => {
     tree = createTreeWithEmptyWorkspace()
@@ -14,10 +14,36 @@ describe('anchor-template generator', () => {
   it.each(['base', 'counter', 'hello-world'])(
     'should generate files for %s template',
     async (template: AnchorTemplateSchema['template']) => {
-      await anchorTemplateGenerator(tree, { ...options, template: template as AnchorTemplateSchema['template'] })
+      await anchorTemplateGenerator(tree, {
+        ...options,
+        name: template,
+        template: template as AnchorTemplateSchema['template'],
+      })
 
       const contents = getRecursiveFileContents(tree, options.directory)
       expect(contents).toMatchSnapshot()
     },
   )
+  it('should be able to generate two templates side by side', async () => {
+    await anchorTemplateGenerator(tree, {
+      ...options,
+      name: 'counter-one',
+      template: 'counter',
+    })
+    await anchorTemplateGenerator(tree, {
+      ...options,
+      name: 'counter-two',
+      template: 'counter',
+    })
+
+    const contents = getRecursiveFileContents(tree, options.directory)
+    expect(contents).toMatchSnapshot()
+    expect(tree.read(options.directory + '/src/index.ts').toString()).toMatchInlineSnapshot(`
+      "
+      export * from './counter-one-exports'
+
+      export * from './counter-two-exports'
+      "
+    `)
+  })
 })
--- packages/preset-anchor/src/generators/anchor-template/files/base/migrations/deploy.ts.template
+++ packages/preset-anchor/src/generators/anchor-template/files/base/migrations/deploy.ts.template
@@ -2,7 +2,7 @@
 // single deploy script that's invoked from the CLI, injecting a provider
 // configured from the workspace's Anchor.toml.
 
-const anchor = require("@coral-xyz/anchor");
+import * as anchor from '@coral-xyz/anchor';
 
 module.exports = async function (provider) {
   // Configure client to use the provider.
