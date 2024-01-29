69b52c2e121d7ea75f868b2be02bf77a9161395f
Bram Borggreve <beeman@beeman.nl>
2024-01-29 16:36:44 +0000

    fix: fix the Nx warning about project name and root

--- packages/preset-react/src/utils/normalize-react-application-schema.ts
+++ packages/preset-react/src/utils/normalize-react-application-schema.ts
@@ -1,16 +1,20 @@
 import { ReactApplicationSchema } from '../generators/react-application/react-application-schema'
 
-export type NormalizedReactApplicationSchema = Required<ReactApplicationSchema>
+export type NormalizedReactApplicationSchema = Required<ReactApplicationSchema> & {
+  directory: string
+}
 
 export type ReactApplicationUi = ReactApplicationSchema['ui']
 export function normalizeReactApplicationSchema(options: ReactApplicationSchema): NormalizedReactApplicationSchema {
+  const webName = options.webName ?? options.name
   return {
     ...options,
     anchor: options.anchor,
     anchorName: options.anchorName ?? 'anchor',
+    directory: webName,
     port: options.port ?? 3000,
     skipFormat: options.skipFormat ?? false,
     ui: options.ui ?? 'tailwind',
-    webName: options.webName ?? options.name,
+    webName,
   }
 }
