5b0f6c87d9e09374c584f28f875d2ec83e96e057
beeman <beeman@beeman.nl>
2024-02-23 18:27:42 +0000

    fix: fix the Nx warning about project name and root (#37)

--- packages/preset-next/src/utils/normalize-next-application-schema.ts
+++ packages/preset-next/src/utils/normalize-next-application-schema.ts
@@ -1,16 +1,20 @@
 import { NextApplicationSchema } from '../generators/next-application/next-application-schema'
 
-export type NormalizedNextApplicationSchema = Required<NextApplicationSchema>
+export type NormalizedNextApplicationSchema = Required<NextApplicationSchema> & {
+  directory: string
+}
 
 export type NextApplicationUi = NextApplicationSchema['ui']
 export function normalizeNextApplicationSchema(options: NextApplicationSchema): NormalizedNextApplicationSchema {
+  const webName = options.webName ?? options.name
   return {
     ...options,
     anchor: options.anchor,
     anchorName: options.anchorName ?? 'anchor',
+    directory: webName,
     port: options.port ?? 3000,
     skipFormat: options.skipFormat ?? false,
     ui: options.ui ?? 'tailwind',
-    webName: options.webName ?? options.name,
+    webName,
   }
 }
