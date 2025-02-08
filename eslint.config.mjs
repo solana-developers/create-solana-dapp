d78307749dcdcaa8d93aa441b00d147d2d2356de
Bram Borggreve <beeman@beeman.nl>
2025-02-08 22:42:47 +0000

    chore: update pnpm version and dev dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,7 @@ export default unjs({
   ],
   rules: {
     // rule overrides
+    '@typescript-eslint/no-require-imports': 'off',
     'unicorn/no-array-reduce': 'off',
     'unicorn/no-process-exit': 'off',
     'unicorn/prefer-top-level-await': 'off',
