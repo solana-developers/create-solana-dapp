8af8aa436ca72e4b5792011a6e06b8bd82d1c11a
Bram Borggreve <beeman@beeman.nl>
2025-04-15 15:36:31 +0100

    chore: update templates

--- src/templates/frameworks.ts
+++ src/templates/frameworks.ts
@@ -13,20 +13,20 @@ export const frameworks: Framework[] = [
     name: 'Next.js',
     description: 'A React framework by Vercel',
     templates: [
-      {
-        name: 'next-tailwind-counter',
-        description: 'Next.js + Tailwind CSS + Anchor Counter Example',
-        repository: 'gh:solana-developers/solana-templates/templates/legacy-next-tailwind-counter',
-      },
-      {
-        name: 'next-tailwind-basic',
-        description: 'Next.js + Tailwind CSS + Anchor Basic Example',
-        repository: 'gh:solana-developers/solana-templates/templates/legacy-next-tailwind-basic',
-      },
+      // {
+      //   name: 'next-tailwind-counter',
+      //   description: 'Next.js + Tailwind CSS + Anchor Counter Example',
+      //   repository: 'gh:solana-developers/solana-templates/templates/template-next-tailwind-counter',
+      // },
+      // {
+      //   name: 'next-tailwind-basic',
+      //   description: 'Next.js + Tailwind CSS + Anchor Basic Example',
+      //   repository: 'gh:solana-developers/solana-templates/templates/template-next-tailwind-basic',
+      // },
       {
         name: 'next-tailwind',
         description: 'Next.js + Tailwind CSS, no Anchor',
-        repository: 'gh:solana-developers/solana-templates/templates/legacy-next-tailwind',
+        repository: 'gh:solana-developers/solana-templates/templates/template-next-tailwind',
       },
     ],
   },
@@ -52,20 +52,57 @@ export const frameworks: Framework[] = [
     name: 'React with Vite',
     description: 'React with Vite and React Router',
     templates: [
-      {
-        name: 'react-vite-tailwind-counter',
-        description: 'React with Vite + Tailwind CSS + Anchor Counter Example',
-        repository: 'gh:solana-developers/solana-templates/templates/legacy-react-vite-tailwind-counter',
-      },
-      {
-        name: 'react-vite-tailwind-basic',
-        description: 'React with Vite + Tailwind CSS + Anchor Basic Example',
-        repository: 'gh:solana-developers/solana-templates/templates/legacy-react-vite-tailwind-basic',
-      },
+      // {
+      //   name: 'react-vite-tailwind-counter',
+      //   description: 'React with Vite + Tailwind CSS + Anchor Counter Example',
+      //   repository: 'gh:solana-developers/solana-templates/templates/template-react-vite-tailwind-counter',
+      // },
+      // {
+      //   name: 'react-vite-tailwind-basic',
+      //   description: 'React with Vite + Tailwind CSS + Anchor Basic Example',
+      //   repository: 'gh:solana-developers/solana-templates/templates/template-react-vite-tailwind-basic',
+      // },
       {
         name: 'react-vite-tailwind',
         description: 'React with Vite + Tailwind CSS, no Anchor',
-        repository: 'gh:solana-developers/solana-templates/templates/legacy-react-vite-tailwind',
+        repository: 'gh:solana-developers/solana-templates/templates/template-react-vite-tailwind',
+      },
+    ],
+  },
+  {
+    id: 'legacy',
+    name: 'Legacy templates',
+    description: 'Templates using @solana/web3.js',
+    templates: [
+      {
+        name: 'legacy-next-tailwind-counter',
+        description: 'Next.js + Tailwind CSS + Anchor Counter Example',
+        repository: 'gh:solana-developers/solana-templates/legacy/legacy-next-tailwind-counter',
+      },
+      {
+        name: 'legacy-next-tailwind-basic',
+        description: 'Next.js + Tailwind CSS + Anchor Basic Example',
+        repository: 'gh:solana-developers/solana-templates/legacy/legacy-next-tailwind-basic',
+      },
+      {
+        name: 'legacy-next-tailwind',
+        description: 'Next.js + Tailwind CSS, no Anchor',
+        repository: 'gh:solana-developers/solana-templates/legacy/legacy-next-tailwind',
+      },
+      {
+        name: 'legacy-react-vite-tailwind-counter',
+        description: 'React with Vite + Tailwind CSS + Anchor Counter Example',
+        repository: 'gh:solana-developers/solana-templates/legacy/legacy-react-vite-tailwind-counter',
+      },
+      {
+        name: 'legacy-react-vite-tailwind-basic',
+        description: 'React with Vite + Tailwind CSS + Anchor Basic Example',
+        repository: 'gh:solana-developers/solana-templates/legacy/legacy-react-vite-tailwind-basic',
+      },
+      {
+        name: 'legacy-react-vite-tailwind',
+        description: 'React with Vite + Tailwind CSS, no Anchor',
+        repository: 'gh:solana-developers/solana-templates/legacy/legacy-react-vite-tailwind',
       },
     ],
   },
